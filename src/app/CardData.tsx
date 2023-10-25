4b102944a198689fc231df3c7e2a5dc587383667
A. Matías Quezada <amatiasq@gmail.com>
2023-10-25 22:47:09 +0200

    Card aspect ratio is now respected

--- src/app/CardData.tsx
+++ src/app/CardData.tsx
@@ -1,10 +1,6 @@
 import { DeckEntry } from '../types/Deck';
 import './CardData.css';
-import { CardEffect } from './CardEffect';
 import { CardView } from './CardView';
-
-const hasEffects = `${document.location}`.includes('effects');
-const MaybeEffects = hasEffects ? CardEffect : 'div';
 
 export interface CardViewProps {
   card: DeckEntry;
@@ -14,11 +10,11 @@ export interface CardViewProps {
 
 export function CardData({ card, large = false, onClick }: CardViewProps) {
   return (
-    <MaybeEffects className="card-data" onClick={onClick}>
+    <div className="card-data" onClick={onClick}>
       <CardView card={card} large={large} />
       <code className="id">{card.id}</code>
       <span className="emojis">{card.emojis}</span>
       {card.count != 1 ? <span className="amount">{card.count}</span> : null}
-    </MaybeEffects>
+    </div>
   );
 }
--- src/app/CardView.tsx
+++ src/app/CardView.tsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from 'react';
 import { Card } from '../types/Card';
+import { CardEffect } from './CardEffect';
 import './CardView.css';
+
+const hasEffects = `${document.location}`.includes('effects');
+const MaybeEffects = hasEffects ? CardEffect : 'div';
 
 export function CardView({ card, large }: { card: Card; large?: boolean }) {
   return (
-    <div className="card">
+    <MaybeEffects className="card">
       {large ? (
         <ScaledImage small={card.images.small} large={card.images.large} />
       ) : (
         <img src={card.images.small} />
       )}
-    </div>
+    </MaybeEffects>
   );
 }
 
