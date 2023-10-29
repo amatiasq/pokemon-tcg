5eb62b38091b410bc3a7fb3a4abcc7fb57f0cd3c
A. Matías Quezada <amatiasq@gmail.com>
2023-10-29 11:54:34 +0100

    Add routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,6 @@
+import { createHashHistory } from 'history';
+import { ComponentChild } from 'preact';
+import Router, { CustomHistory } from 'preact-router';
 import decks from 'virtual:all-decks';
 import { setFocusedCard } from '../hooks/useFocusedCard';
 import './App.css';
@@ -9,24 +12,45 @@ import { FocusedCard } from './FocusedCard';
 export function App() {
   return (
     <>
+      <aside>
+        <a href="/">Decks</a>
+        <a href="/build">Build</a>
+        <a href="/filters">Filters</a>
+      </aside>
+
       <main>
-        {decks.map((deck) => (
-          <DeckView key={deck.name} deck={deck}>
-            {(card) => (
-              <CardData
-                card={card}
-                onClick={(event) => {
-                  const target = event.target as HTMLDivElement;
-                  setFocusedCard(card, target.closest('.card-data')!);
-                }}
-              />
-            )}
-          </DeckView>
-        ))}
-        <DeckBuilder />
+        <Router history={createHashHistory() as unknown as CustomHistory}>
+          <Path path="/" component={<Decks />} />
+          <Path path="/build" component={<DeckBuilder />} />
+          <Path path="/filters" component={<p>TODO</p>} />
+        </Router>
       </main>
 
       <FocusedCard />
+    </>
+  );
+}
+
+function Path({ component }: { path: string; component: ComponentChild }) {
+  return <>{component}</>;
+}
+
+function Decks() {
+  return (
+    <>
+      {decks.map((deck) => (
+        <DeckView key={deck.name} deck={deck}>
+          {(card) => (
+            <CardData
+              card={card}
+              onClick={(event) => {
+                const target = event.target as HTMLDivElement;
+                setFocusedCard(card, target.closest('.card-data')!);
+              }}
+            />
+          )}
+        </DeckView>
+      ))}
     </>
   );
 }
