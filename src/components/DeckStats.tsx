98a6f836a1c5a75063a75177756553ca1a88702f
A. Matías Quezada <amatiasq@gmail.com>
2023-10-29 02:35:00 +0200

    Missing file

---
+++ src/components/DeckStats.tsx
@@ -0,0 +1,27 @@
+import { DeckEntry } from 'types:Deck';
+import { toggleFilter, useCardFilters } from '../hooks/useCardFilters';
+import './DeckStats.css';
+
+export function DeckStats({ cards }: { cards: DeckEntry[] }) {
+  const { isExcluded } = useCardFilters();
+
+  const emojis: Record<string, number> = {};
+
+  for (const card of cards) {
+    for (const emoji of card.emojis) {
+      emojis[emoji] = (emojis[emoji] || 0) + card.count;
+    }
+  }
+
+  return (
+    <ul className="deck-stats">
+      {Object.entries(emojis).map(([emoji, count]) => (
+        <li key={emoji} className={isExcluded(emoji) ? 'excluded' : ''}>
+          <button onClick={() => toggleFilter(emoji)}>
+            {emoji} ({count})
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
